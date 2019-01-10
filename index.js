537999fb3c2eb0b70da4c79c91131b0665faefa7
Yi-Wei Hsu <yiwei@dots.de>
2019-01-10 17:48:17 +0100

    small fix with prettier

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import schema from './schema/schema';
 const app = express();
 
 app.use(
-  '/graphql',
-  expressGraphQL({
-    schema,
-    graphiql: true,
-  })
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	}),
 );
 
 app.listen(4000, () => {
-  console.log('Listening on the port 4000');
+	console.log('Listening on the port 4000');
 });
